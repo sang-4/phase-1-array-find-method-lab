135b3b93122f6d95bf28315944866ef2e52fc69d
Allan <kipropsallan@gmail.com>
2022-06-12 18:53:44 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin(objArr)
+{
+    let firstWin
+    objArr.find(function(obj) {
+        if (obj.result === "W")
+        {
+         firstWin = obj.year
+         return firstWin;
+        }
+    })
+    return firstWin
+}         
